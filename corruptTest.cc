971f0f5ccc408f645f33e6bfda147e8b682eedff
ben <benjaminjritz@gmail.com>
2019-02-18 15:54:15 -0500

    added independent corruption test

---
+++ corruptTest.cc
@@ -0,0 +1,23 @@
+#include "parse_scores.h"
+
+#include <cassert>
+#include <string>
+#include <iostream>
+using std::cin;
+using std::cout;
+using std::endl;
+
+std::string values[] = {
+  "3",
+  "1234", "3", "99.1", "87.92", "77.32",
+  "2345", "4", "93.1", "99.6", "81.21", "89.33",
+  "3456", "5", "99.3", "99.3", "83.49", "76.48", "99.6"};
+int size = 19;
+
+int main(int argc, char* argv[]){
+
+
+
+
+    return 0;
+}//ending bracket of main method
